04d36a0c48cf5da10194bf1c0ecd47bc16ea4ffd
Spikefire63 <klitschmark@gmail.com>
2026-05-04 23:34:42 -0400

    Revert "Update Dirt_Track_Racing.Build.cs"
    This reverts commit c573382875cae25f72029a461ddda0699f7f6edf.

--- Source/Dirt_Track_Racing/Dirt_Track_Racing.Build.cs
+++ Source/Dirt_Track_Racing/Dirt_Track_Racing.Build.cs
@@ -8,7 +8,7 @@ public class Dirt_Track_Racing : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ChaosVehicles" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
