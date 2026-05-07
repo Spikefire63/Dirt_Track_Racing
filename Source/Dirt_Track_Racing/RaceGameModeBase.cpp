7258928d2c537fc02ffe037cac14ccb58a094ccd
Spikefire63 <klitschmark@gmail.com>
2026-05-07 12:42:00 -0400

    Fixed crash
    Default pawn was not set correctly

--- Source/Dirt_Track_Racing/RaceGameModeBase.cpp
+++ Source/Dirt_Track_Racing/RaceGameModeBase.cpp
@@ -3,8 +3,10 @@
 
 #include "RaceGameModeBase.h"
 #include "VehiclePlayerController.h"
+#include "OffroadPawn.h"
 
 ARaceGameModeBase::ARaceGameModeBase()
 {
+	DefaultPawnClass = AOffroadPawn::StaticClass();
 	PlayerControllerClass = AVehiclePlayerController::StaticClass();
 }
